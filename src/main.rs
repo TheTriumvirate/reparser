ae6436d69d3834953924dd0220a4d13a66da704c
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-10-07 00:06:34 +0200

    Interpret data by filtering by/including FA value

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ extern crate serde;
 extern crate bincode;
 extern crate nalgebra as na;
 
+extern crate colored; // warnings
+
+use colored::*;
 use std::fs::File;
 use std::io::prelude::*;
 use nom::{be_f32, le_f32};
@@ -40,8 +43,7 @@ struct Opt {
 
     /// Depth of the model
     #[structopt(short = "d", long = "depth")]
-    depth: usize,
-
+    depth: usize, 
     /// Output file
     #[structopt(short = "o", long = "output", parse(from_os_str), default_value = "./out.bincode")]
     output: PathBuf,
@@ -51,7 +53,7 @@ struct Opt {
     file: PathBuf,
 }
 
-pub type Field = Vec<Vec<Vec<(f32,f32,f32)>>>;
+pub type Field = Vec<Vec<Vec<(f32, f32,f32,f32)>>>;
 
 #[derive(Serialize, Deserialize)]
 pub struct VectorField {
@@ -65,25 +67,29 @@ impl VectorField {
     pub fn from_eigenanalysis(width: usize, height: usize, depth: usize, values: &Vec<f32>) -> Self {
         // difference between largest vector and the others, used as a measure
         // of whether the data is useful or not
-        const EPSILON : f32 = 0.000001;
+        const FA_EPSILON : f32 = 0.2;
         const NDIM : usize = 7;
         
+        let mut index: usize = 0;
         let mut data : Field = Vec::new();
-        for i in 0..depth {
+        for ez in 0..depth {
             let mut plane = Vec::new();
-            for j in 0..height {
+            for ey in 0..height {
                 let mut row = Vec::new();
-                for k in 0..width {
-                    let confidence: f32 = values[i*height*width*NDIM+j*width*NDIM+k*NDIM+0];
-                    let dxx : f32 = values[i*height*width*NDIM+j*width*NDIM+k*NDIM+1];
-                    let dxy : f32 = values[i*height*width*NDIM+j*width*NDIM+k*NDIM+2];
-                    let dxz : f32 = values[i*height*width*NDIM+j*width*NDIM+k*NDIM+3];
-                    let dyy : f32 = values[i*height*width*NDIM+j*width*NDIM+k*NDIM+4];
-                    let dyz : f32 = values[i*height*width*NDIM+j*width*NDIM+k*NDIM+5];
-                    let dzz : f32 = values[i*height*width*NDIM+j*width*NDIM+k*NDIM+6];
+                for ex in 0..width {
+                    index = ez*width*height*NDIM + ey*width*NDIM + ex*NDIM;
+                    let confidence: f32 = values[index];
+                    let dxx : f32 = values[index+1];
+                    let dxy : f32 = values[index+2];
+                    let dxz : f32 = values[index+3];
+                    let dyy : f32 = values[index+4];
+                    let dyz : f32 = values[index+5];
+                    let dzz : f32 = values[index+6];
                     let mut x:f32 = 0.0;
                     let mut y:f32 = 0.0;
                     let mut z:f32 = 0.0;
+
+                    let mut fa:f32 = 0.0;
                     
                     if confidence == 1.0 {
                         let ds : Matrix3<f32> = Matrix3::new(dxx,dxy,dxz,
@@ -92,30 +98,34 @@ impl VectorField {
                         let mut res = ds.symmetric_eigen();
                         let mut ev_sorted = res.eigenvalues.iter().map(|&f| f.abs()).collect::<Vec<f32>>();
                         ev_sorted.sort_by(|&a,&b| a.partial_cmp(&b).unwrap());
-                        let max = ev_sorted.pop().unwrap();
-                        let not_max = ev_sorted.pop().unwrap();
-                        //println!("MAX: {}", max);
-                        //println!("NOT_MAX: {}", not_max);
-                        //println!("EIG: {:?}", res.eigenvalues);
-                        if max-not_max > EPSILON { // needle shaped tensor
+                        let l1 = ev_sorted.pop().unwrap();
+                        let l2 = ev_sorted.pop().unwrap();
+                        let l3 = ev_sorted.pop().unwrap();
+                        // fa value
+                        fa = ((l1-l2).powf(2.0) + (l1-l3).powf(2.0) + (l2-l3).powf(2.0)).sqrt()/
+                                 (2.0*(l1.powf(2.0)+l2.powf(2.0)+l3.powf(2.0))).sqrt();
+                        if fa > FA_EPSILON { // needle shaped tensor
                             let a = res.eigenvalues.iamax_full();
-                            //println!("IAMAX: {:?}", a.0);
-                            //println!("RES: {}", res.eigenvectors);
                             let most_significant_vector = res.eigenvectors.column(a.0);
                             //                         row  col
-                            x = max*most_significant_vector[(0,   0  )];
-                            y = max*most_significant_vector[(1,   0  )];
-                            z = max*most_significant_vector[(2,   0  )];
-                            //println!("x,y,z = {}, {}, {}", x, y, z);
-                            if (x*x+y*y+z*z).sqrt() < EPSILON { x = 0.0; y = 0.0; z = 0.0; }
+                            x = most_significant_vector[(0,   0  )];
+                            y = most_significant_vector[(1,   0  )];
+                            z = most_significant_vector[(2,   0  )];
                         }
                     }
                     
-                    row.push((x,y,z));
+                    row.push((x,y,z,fa));
                 }
                 plane.push(row);
             }
             data.push(plane);
+        }
+        
+        if index+6 < width*height*depth-1 {
+            
+            println!("{} Last index visited was {}, but total number of floats was {}.",
+                     "WARNING:".yellow().bold(), index+6, height*width*depth);
+            println!("{:8} You probably fucked up how you do the dimensions", " ");
         }
         VectorField { width, height, depth, field: data }
     }
